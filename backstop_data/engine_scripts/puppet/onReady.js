f5de8ef6e19431931e9e89fc3380a416cd0be2a3
Antonio Castiglione <antonio@minervawebdevelopment.com>
2022-05-05 16:23:10 -0400

    tweaked page eval so it sets header statically and removes popup if found

--- backstop_data/engine_scripts/puppet/onReady.js
+++ backstop_data/engine_scripts/puppet/onReady.js
@@ -4,13 +4,23 @@ module.exports = async (page, scenario, vp) => {
 
   // add more ready handlers here...
   await page.evaluate(async () => {
+    // Set header to static so it doesn't capture incorrectly.
+    document.querySelector("header.site-header").style.cssText =
+      "; transform: translateY(0px); position: static;";
+    document.querySelector("#content .col-full").style.cssText = "; margin-top: 0;";
+
     await new Promise((resolve, reject) => {
       var totalHeight = 0;
-      var distance = 200;
+      var distance = 300;
       var timer = setInterval(() => {
         var scrollHeight = document.body.scrollHeight;
         window.scrollBy(0, distance);
         totalHeight += distance;
+        var popup = document.querySelector(".needsclick");
+
+        if (popup) {
+          popup.remove();
+        }
 
         if (totalHeight >= scrollHeight) {
           clearInterval(timer);
